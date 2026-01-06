f87032cc8e3f7333d465d83037bf2c01654172c3
Nuttachai-Su <lasernaball@gmail.com>
2026-01-07 01:25:24 +0700

    Complete

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,28 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool p[][M]){
+    for (int j = 0; j <= M+1; ++j) cout << "-";
+    cout << "\n";
+    for (int i = 0; i < N; ++i) {
+        cout << "|";
+        for (int j = 0; j < M; ++j) {
+            if (p[i][j] == 0) cout << (" ");
+            else if (p[i][j] == 1) cout << ("*"); 
+        }
+        cout << "|" << "\n";
+    }
+    for (int j = 0; j <= M+1; ++j) cout << "-";
+}
+
+void updateImage(bool p[N][M],int s, int x, int y){
+    if (s <= 0) return;
+    int r = s - 1;
+    int r2 = r * r;
+    int i_min = max(0,     x - r);
+    int i_max = min(N - 1, x + r);
+    int j_min = max(0,     y - r);
+    int j_max = min(M - 1, y + r);
+
+}
