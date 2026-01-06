b1a1e670f53ce325c9a2f73213f5679895a96655
Nuttachai-Su <lasernaball@gmail.com>
2026-01-06 20:38:57 +0700

    complete

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,25 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double a[],int n,double b[]){
+    for (int i=0; i<n; i++) b[0] += a[i]; //Arithmetic Mean
+    b[0] = b[0] / n;
+
+    for (int i=0; i<n; i++) b[1] += pow(a[i] - b[0], 2); //Standard Deviation
+    b[1] = b[1] / n;
+    b[1] = pow(b[1],0.5);
+
+    b[2] = 1; //Geometric Mean
+    for (int i=0; i<n; i++) b[2] *= a[i];
+    b[2] = pow(b[2],1.0/n);
+
+    for (int i=0; i<n; i++) b[3] += 1/a[i]; //Harmonic Mean
+    b[3] = n / b[3];
+
+    for (int i=0; i<n; i++) if(b[4]<a[i]) b[4] = a[i];
+
+    b[5] = a[0];
+    for (int i=0; i<n; i++) if(b[5]>a[i]) b[5] = a[i];
+
+}
